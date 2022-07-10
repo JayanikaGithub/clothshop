8c13279b0daa4aafba9fdd2d522137e5c6ad120e
JayanikaGithub <AR96588@fhss.sjp.ac.lk>
2022-07-10 13:03:47 +0530

    edit files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-const App = () => {
-  return <div>Hello world</div>
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+const App= () => {
+  return <Home/>
 };
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,81 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://o.remove.bg/downloads/bd823498-de44-45e3-b222-830915901ba3/21993022_1365755570203466_2683410099515220765_o-removebg-preview.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "faabb9",
+    },
+    {
+      id: 2,
+      img: "https://i.ibb.co/DG69bQ4/2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "bfb2f3",
+    },
+    {
+      id: 3,
+      img: "https://i.ibb.co/cXFnLLV/3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "faabb9",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://scontent.fcmb1-2.fna.fbcdn.net/v/t45.5328-4/282377507_5070059073078507_8905825957736851796_n.jpg?stp=dst-jpg_p960x960&_nc_cat=103&ccb=1-7&_nc_sid=c48759&_nc_ohc=_NvlIvaBIWkAX_5otpP&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT_6oWe2fnPesTz6Os1gGvGtFTqrlhkh-Jt6nEEnWkXq_A&oe=62CE4908",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "https://scontent.fcmb1-2.fna.fbcdn.net/v/t45.5328-4/283493400_7489587521083218_2824616072587987751_n.jpg?stp=dst-jpg_p960x960&_nc_cat=108&ccb=1-7&_nc_sid=c48759&_nc_ohc=8y5-RGrw6FsAX8f2UjJ&tn=1PyL_p61C6NLNxcf&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT9ymP37yo1HbXmtrfz0MDocc7nH5DE6IBriZngU9JKU5g&oe=62CEE40D",
+      title: "OFFICE WEAR",
+    },
+    {
+      id: 3,
+      img: "https://scontent.fcmb1-2.fna.fbcdn.net/v/t45.5328-4/286409360_5330520680303647_3594611637901910975_n.jpg?stp=dst-jpg_p960x960&_nc_cat=108&ccb=1-7&_nc_sid=c48759&_nc_ohc=MkbUc7VQby4AX9axwzj&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT9ZxlL1Ji06SWr_aHXdpj5a8hb-nhYEig451CIQLQ1xHA&oe=62CF10CF",
+      title: "PARTY WEAR",
+    },
+    {
+      id: 3,
+      img: "https://scontent.fcmb1-2.fna.fbcdn.net/v/t39.30808-6/290417548_5039727309472922_6941759171909079969_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=hQWi1NFqO68AX9oKbs2&_nc_ht=scontent.fcmb1-2.fna&oh=00_AT_YUy_avZaxUK3mFQl-90F06vPL227gWUyzduIGLzoUQg&oe=62CFDCEB",
+      title: "CASUAL WEAR",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://www.topofstyle.com/blog/wp-content/uploads/2021/02/sweatyrocks-short-sleeve-crop-top-tshirt.jpg",
+    },
+    {
+      id:2,
+      img:"https://purepng.com/public/uploads/large/purepng.com-helly-hansen-daybreaker-fleece-jacket-womengarmentupper-bodyjacketlighterladieshelly-hansendaybreakerfleece-142152636173585qyc.png",
+    },
+    {
+      id:3,
+      img:"https://i.pinimg.com/474x/74/28/25/7428251e42c8186b4163f463aa59075d.jpg",
+    },
+    {
+      id:4,
+      img:"https://www.pngmart.com/files/6/Blouse-Transparent-Images-PNG.png",
+    },
+    {
+      id:5,
+      img:"https://img3.junaroad.com/uiproducts/17371475/pri_175_p-1603726347.jpg",
+    },
+    {
+      id:6,
+      img:"https://www.pngall.com/wp-content/uploads/7/Blouse.png",
+    },
+    {
+      id:7,
+      img:"https://m.media-amazon.com/images/I/51l9kwCU1WL._UX569_.jpg",
+    },
+    {
+      id:8,
+      img:"https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+    },
+  ]
